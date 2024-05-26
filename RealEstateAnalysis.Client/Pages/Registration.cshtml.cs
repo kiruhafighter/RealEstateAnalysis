4f27f21edefdf9ca792b19a1853c9bf188f15ba8
kiruhafighter <salomatink2@gmail.com>
2024-05-27 00:00:54 +0300

    Register first version

---
+++ RealEstateAnalysis.Client/Pages/Registration.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using ApiClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstateAnalysis.Client.Pages;
+
+public class RegistrationModel : PageModel
+{
+    [BindProperty]
+    public AddUserDto AddUserDto { get; set; } = new AddUserDto();
+    
+    public string? ErrorMessage { get; set; }
+    
+    private readonly IClient _client;
+
+    public RegistrationModel(IClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            ErrorMessage = "Invalid data";
+            return Page();
+        }
+
+        try
+        {
+            await _client.RegisterUserAsync(AddUserDto, default);
+            return RedirectToPage("/Login");
+        }
+        catch (ApiException<string> ex)
+        {
+            ErrorMessage = ex.Result;
+            return Page();
+        }
+    }
+}
