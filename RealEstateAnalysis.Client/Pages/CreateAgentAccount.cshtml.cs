89ef934118ae22c7549066073f0dc8095c9219da
kiruhafighter <salomatink2@gmail.com>
2024-06-03 17:44:35 +0300

    Creat agent is done

---
+++ RealEstateAnalysis.Client/Pages/CreateAgentAccount.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using ApiClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstateAnalysis.Client.Pages;
+
+public class CreateAgentAccountModel : PageModel
+{
+    private readonly IClient _client;
+
+    public CreateAgentAccountModel(IClient client)
+    {
+        _client = client;
+    }
+
+    [BindProperty]
+    public AddAgentDto AddAgentDto { get; set; } = new();
+    
+    public string? ErrorMessage { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            ErrorMessage = "Invalid data";
+            return Page();
+        }
+
+        try
+        {
+            await _client.AddAgentAccountAsync(AddAgentDto);
+            return RedirectToPage("/Index");
+        }
+        catch(ApiException<string> ex)
+        {
+            ErrorMessage = ex.Result;
+            return Page();
+        }
+    }
+}
