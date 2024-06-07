0370962f4abdf695211c129ac088f0f053583a9c
kiruhafighter <salomatink2@gmail.com>
2024-06-07 19:50:02 +0300

    The project MVP is done!

---
+++ RealEstateAnalysis.Client/Pages/Logout.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstateAnalysis.Client.Pages;
+
+public class LogoutModel : PageModel
+{
+    public IActionResult OnGet()
+    {
+        // Clear the jwtToken cookie
+        Response.Cookies.Delete("jwtToken");
+
+        // Redirect to home page
+        return RedirectToPage("/Index");
+    }
+}
---
+++ RealEstateAnalysis.Client/Utils/NavBarViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateAnalysis.Client.Utils;
+
+public sealed class NavBarViewComponent : ViewComponent
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public NavBarViewComponent(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    
+    public IViewComponentResult Invoke()
+    {
+        var isAuthenticated = IsUserAuthenticated();
+        return View(isAuthenticated);
+    }
+
+    private bool IsUserAuthenticated()
+    {
+        var token = GetTokenFromCookie();
+        if (string.IsNullOrEmpty(token))
+        {
+            return false;
+        }
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwtToken = handler.ReadJwtToken(token);
+        var userRoleClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Role);
+
+        return userRoleClaim != null;
+    }
+
+    private string? GetTokenFromCookie()
+    {
+        var httpContext = _httpContextAccessor.HttpContext;
+        if (httpContext != null && httpContext.Request.Cookies.ContainsKey("jwtToken"))
+        {
+            return httpContext.Request.Cookies["jwtToken"];
+        }
+
+        return null;
+    }
+}
